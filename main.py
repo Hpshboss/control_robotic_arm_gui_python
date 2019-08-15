9436896f90bd7634f856ef4ac8ed20be282a1596
Hpshboss <s9303935522@gmail.com>
2019-08-15 11:40:38 +0800

    read last line fo textEdit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import serial
 import sys
+import os
 from PyQt5.QtWidgets import QMainWindow
 import GUI
 from PyQt5.QtWidgets import QApplication
@@ -11,8 +12,8 @@ class Main(QMainWindow, GUI.Ui_MainWindow):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.import_pushbutton)
         self.pushButton_2.clicked.connect(self.enter_pushbutton)
-        self.pushButton_3.clicked.connect(self.stop_pushbutton)
-        self.pushButton_4.clicked.connect(self.g_pushbutton)
+        self.pushButton_4.clicked.connect(self.stop_pushbutton)
+        self.pushButton_3.clicked.connect(self.g_pushbutton)
 
     def import_pushbutton(self):
         print("Import button")
@@ -28,17 +29,41 @@ class Main(QMainWindow, GUI.Ui_MainWindow):
         text_buffer_list = text_buffer.splitlines()
         print(text_buffer_list[-1])
         try:
-            robotic_arm = serial.Serial("COM", 9600)
-            robotic_arm.write(self.textEdit.toPlainText())
-            robotic_arm.read(50)
+            robotic_arm = serial.Serial("COM8", 9600, xonxoff=True, timeout= 0.5)
+            robotic_arm.write(self.textEdit.toPlainText().encode() + os.linesep.encode())
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
         except:
             print("failed enter")
 
     def stop_pushbutton(self):
         print("Stop button")
+        try:
+            robotic_arm = serial.Serial("COM8", 9600, xonxoff=True, timeout=0.5)
+            robotic_arm.write("EXIT".encode() + os.linesep.encode())
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+        except:
+            print("failed Stop")
 
     def g_pushbutton(self):
         print("G button")
+        try:
+            robotic_arm = serial.Serial("COM8", 9600, xonxoff=True, timeout=0.5)
+            robotic_arm.write("G".encode() + os.linesep.encode())
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+            print(robotic_arm.readline(50))
+        except:
+            print("failed G")
 
 
 if __name__ == "__main__":
